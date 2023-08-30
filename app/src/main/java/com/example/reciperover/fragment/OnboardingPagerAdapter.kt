bb030b4c966fb9e3d29b33fae761a11f157bbdbd
Jatin Khatri <jatinkhatri245@gmail.com>
2023-08-30 14:40:37 +0530

    Update OnboardingPagerAdapter.kt

--- app/src/main/java/com/example/reciperover/fragment/OnboardingPagerAdapter.kt
+++ app/src/main/java/com/example/reciperover/fragment/OnboardingPagerAdapter.kt
@@ -11,7 +11,7 @@ class OnboardingPagerAdapter(fragmentActivity: FragmentActivity): FragmentStateA
         Page("Cook Like a Chef","Impress your friends and family with delicious homemade meals",R.drawable.ic_first_slide),
         Page("Explore Recipes", "Browse through our extensive collection of mouthwatering recipes", R.drawable.second_slide)
     )
- //   private var dotIndicatorViews = mutableListOf<ImageView>()
+ 
 
     override fun getItemCount(): Int = pages.size
 
@@ -20,26 +20,4 @@ class OnboardingPagerAdapter(fragmentActivity: FragmentActivity): FragmentStateA
         return OnboardingPageFragment.newInstance(page.title, page.description, page.imageResId)
     }
 
-//    fun setUpDotIndicators(dotIndicatorLayout: LinearLayout) {
-//        val context = dotIndicatorLayout.context
-//        dotIndicatorViews.clear()
-//        dotIndicatorLayout.removeAllViews()
-//        for (i in pages.indices) {
-//            val dotImageView = ImageView(context)
-//            dotImageView.setImageResource(R.drawable.ic_dot_unselected)
-//            dotIndicatorViews.add(dotImageView)
-//            dotIndicatorLayout.addView(dotImageView)
-//        }
-//        selectDotIndicator(0)
-//    }
-//
-//    fun selectDotIndicator(position: Int) {
-//
-//        for((index, dotImageView) in dotIndicatorViews.withIndex()) {
-//            dotImageView.setImageResource(
-//                if (index == position) R.drawable.ic_dot_selected else R.drawable.ic_dot_unselected
-//            )
-//        }
-//
-//    }
 }
